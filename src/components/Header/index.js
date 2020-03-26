09e2c861a7676ef1d08aad51418bfb4dd933cb99
steven-zhao <zhwdyx1201@gmail.com>
2020-03-26 17:04:35 +0800

    change header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,8 +5,8 @@ import { Col, Row } from "antd"
 const Header = props => {
   return (
     <Row className="header-content" justify="space-between" align="middle">
-      <Col span={4}>
-        <Link to="/">Home Logo</Link>
+      <Col span={4} className="logo-group">
+        <Link to="/">Tomato Notes</Link>
       </Col>
       <Col span={10} className="link-group">
         <Link to="/login">Login</Link>
